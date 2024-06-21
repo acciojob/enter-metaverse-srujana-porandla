505aff525efb2ef4db17b793d7a5fd6359ab9b03
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-21 12:53:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener('click', function() {
+    
+    const enterBtn = document.getElementById('enterBtn');
+    const statusParagraph = document.getElementById('status');
+    
+   
+    enterBtn.addEventListener('click', function() {
+       
+        statusParagraph.innerHTML = '<h1>Entered Metaverse</h1>';
+    });
+});
